903391d7f925cd9c539acb6e76b36c19b1d047f8
F-Unction <f_unction@qq.com>
2020-08-19 17:31:17 +0800

    readme

---
+++ ExDirectUI.NET/Frameworks/Layout/ExBaseLayout.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using ExDirectUI.NET.Native;
+
+namespace ExDirectUI.NET.Frameworks.Layout
+{
+    class ExBaseLayout : IDisposable
+    {
+        
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -33,7 +33,7 @@ ExDirectUI.NET
 |路径对象|ExPath|√|
 |区域对象|ExRegion|√|
 |字体对象|ExFont|√|
-|缓动对象|ExEasing|×|
+|缓动对象|ExEasing|√|
 |布局对象基类|ExBaseLayout|×|
 |线性布局对象|ExLinearLayout|×|
 |流式布局对象|ExFlowLayout|×|
