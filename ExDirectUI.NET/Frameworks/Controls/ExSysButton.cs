f57627a262802deb05346b373d3c43869360039b
F-Unction <f_unction@qq.com>
2020-08-21 08:08:32 +0800

    and another one

---
+++ ExDirectUI.NET/Frameworks/Controls/ExSysButton.cs
@@ -0,0 +1,12 @@
+﻿namespace ExDirectUI.NET.Frameworks.Controls
+{
+    public class ExSysButton : ExControl
+    {
+        public ExSysButton(IExBaseUIEle oParent, string sTitle, int x, int y, int nWidth, int nHeight)
+            : base(oParent, "SysButton", sTitle, x, y, nWidth, nHeight)
+        {
+        }
+
+        public new string ClassName => "SysButton";
+    }
+}
